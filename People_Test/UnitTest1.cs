73e40464b73d6067450d6d4efa002133da0b6a74
Визуал студио ! <vspc_vs@VSPC34.RU>
2021-03-09 13:37:34 +0300

    паа

---
+++ People_Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectTestExample;
+
+namespace People_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        People people = new People("samctum", "2033", "dima@yandex.ru", 79992647826);
+        [TestMethod]
+        public void GetNameTest()
+        {
+            Assert.AreEqual("samctum", people.Login);
+        }
+        public void GetPassTest()
+        {
+            Assert.AreEqual("2033", people.Pass);
+        }
+        public void GetEmailTest()
+        {
+            Assert.AreEqual("dima@yandex.ru", people.Email);
+        }
+        public void GetPhoneTest()
+        {
+            Assert.AreEqual(79992647826, people.Phone);
+        }
+    }
+}
